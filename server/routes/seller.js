98abf2f7c9b678f9b489baf728af5b4cba82ef93
Hem <hemsunder47@gmail.com>
2020-03-23 07:16:17 +0530

    added faker to generate fake data

--- server/routes/seller.js
+++ server/routes/seller.js
@@ -9,6 +9,7 @@ const s3 = new aws.S3({
   secretAccessKey: "SZPKvHYMA4BV1O+gFFXZl5Ud8EnkC+tpX7GhRfpa"
 });
 const checkJWT = require("../middlewares/jwt-check");
+const faker = require("faker");
 
 const upload = multer({
   storage: multerS3({
@@ -25,29 +26,29 @@ const upload = multer({
 
 router
   .route("/products")
-  .get( checkJWT, (req, res, next) => {
+  .get(checkJWT, (req, res, next) => {
     console.log(req.body);
-    Product.find({ owner: req.decoded.user._id})
-    .populate('owner')
-    .populate('category')
-    .exec( (error, products) => {
-      if(products){
-        res.json({
-          success: true,
-          message: 'Successfully got product information',
-          products: products
-        });
-      }
-    })
+    Product.find({ owner: req.decoded.user._id })
+      .populate("owner")
+      .populate("category")
+      .exec((error, products) => {
+        if (products) {
+          res.json({
+            success: true,
+            message: "Successfully got product information",
+            products: products
+          });
+        }
+      });
   })
   // .post([ checkJWT, upload.single('product_picture')], (req, res, next) => {
-  .post([ checkJWT ], (req, res, next) => {
+  .post([checkJWT], (req, res, next) => {
     console.log(req.body);
     let product = new Product();
     product.owner = req.decoded.user._id;
     product.category = req.body.categoryId;
     // product.image = req.file.location;
-    product.image = 'no-image';
+    product.image = "no-image";
     product.title = req.body.title;
     product.price = req.body.price;
     product.description = req.body.description;
@@ -57,5 +58,24 @@ router
       message: "Successfully added product"
     });
   });
+
+router
+  .route("/faker/test")
+  .get((req, res, next) => {
+    for (i = 0; i < 20; i++) {
+      let product = new Product();
+      product.category = "5e780c31b30f00259eb12c70"; //transport category id
+      product.owner = "5e7443aba7985327e2e5268c"; //hem sunder id
+      product.image = faker.image.cats();
+      product.title = faker.commerce.productName();
+      product.description = faker.lorem.words();
+      product.price = faker.commerce.price();
+      product.save();
+    }
+    res.json({
+      success: true,
+      message: "Successfully added 20 fake data"
+    });
+});
 
 module.exports = router;
