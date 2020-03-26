ced9219c6315e6119989d59db62bff25cd94bfca
Hem <hemsunder47@gmail.com>
2020-03-26 11:06:03 +0530

    getting 10 products per category api created

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const Category = require("../models/category");
+const Product = require("../models/product");
+const async = require("async");
 
 // Categories api
 router
@@ -22,5 +24,41 @@ router
       message: "Category saved successfully"
     });
   });
+
+router.get('/categories/:id', (req, res, next) => {
+  const perPage = 10;
+  const page = req.query.page;
+  async.waterfall([
+    function(callback){
+      Product.countDocuments({ category: req.params.id}, ( error, count ) => {
+        let totalProducts = count;
+        callback(error, totalProducts);
+      });
+    },
+    function(totalProducts, callback){
+      Product.find({ category: req.params.id })
+      .skip( perPage * page )
+      .limit(perPage)
+      .populate('category')
+      .populate('owner')
+      .exec((error, products) => {
+        if(error) return next(error);
+        callback(error,products, totalProducts)
+      });
+    },
+    function(products, totalProducts, callback){
+      Category.findOne({ _id: req.params.id }, ( error, category) => {
+        res.json({
+          success: true,
+          message: '10 items per category found',
+          products: products,
+          categoryName: category.name,
+          totalProducts: totalProducts,
+          pages: Math.ceil(totalProducts / perPage)
+        });
+      });
+    }
+  ]);
+})
 
 module.exports = router;
